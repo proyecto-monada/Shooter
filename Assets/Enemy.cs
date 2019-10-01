532b9cecd77e552d8e4998c9b25dbfe6f0e16e7e
DavidPastorBonilla <DavidPastorBonilla@users.noreply.github.com>
2019-10-01 19:56:32 +0200

    aa

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+public Collider MainColiider;
+public Collider[] AllColliders;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+MainCollider = GetComponent<Collider>();
+AllColliders = GetComponentsInChildren<Collider>(true);
+DoRagdoll(false);
+        
+    }
+
+
+public void DoRagdoll(bool isRagdoll){
+
+foreach (var col in AllColliders)
+	col.enabled = isRagdoll;
+MainCollider.enabled = !isRagdoll;
+GetComponent<Rigidbody>().useGravity=!isRagdoll;
+GetComponent<Animator>().enabled = !isRagdoll;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Generacion.cs
@@ -0,0 +1,33 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generacion : MonoBehaviour
+{
+	public GameObject theEnemy;
+	public float xPos;
+	public float zPos;
+	public float timeGenerator;
+	public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+	IEnumerator EnemyDrop()
+	{
+		while(enemyCount < 100)
+		{
+			xPos = -8;
+			zPos = Random.Range(-5,0);
+			timeGenerator = Random.Range(1,4);
+
+		//	Instantiate(theEnemy, new Vector3(xPos, 0, zPos), Quaternion.identity);
+			Instantiate(theEnemy, new Vector3(xPos, 0, zPos), Quaternion.Euler(0,90,0));
+			yield return new WaitForSeconds(timeGenerator);
+			enemyCount += 1;
+		}
+	}
+
+}
