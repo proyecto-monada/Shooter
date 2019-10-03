eba559b4d2c165e4103935584b8a0c3e314215bd
DavidPastorBonilla <DavidPastorBonilla@users.noreply.github.com>
2019-10-03 13:01:33 +0200

    zombie dies

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          setRigidbodyState(true);
+        setColliderState(false);
+        GetComponent<Animator>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+public void die()
+    {
+
+        GetComponent<Animator>().enabled = false;
+        setRigidbodyState(false);
+        setColliderState(true);
+
+        if (gameObject != null)
+        {
+            Destroy(gameObject, 5f);
+        }
+
+    }
+
+void setRigidbodyState(bool state)
+    {
+
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+
+        GetComponent<Rigidbody>().isKinematic = !state;
+
+    }
+
+
+    void setColliderState(bool state)
+    {
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+GetComponent<Collider>().enabled = !state;
+
+}
+}
