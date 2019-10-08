18572624e4395a32c30ccdca61c7a18dfe389aec
PedroAcero <55836368+PedroAcero@users.noreply.github.com>
2019-10-08 23:18:58 +0200

    Cronometro

---
+++ Assets/Scripts/Temporizador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+
+public class Temporizador : MonoBehaviour
+{
+	public Text contador;
+	public float tiempo = 0.0f;
+
+	
+	public void Update() 
+	{
+		tiempo -= Time.deltaTime; 
+		contador.text = "" + tiempo.ToString("f0");	
+	}
+	
+
+}
