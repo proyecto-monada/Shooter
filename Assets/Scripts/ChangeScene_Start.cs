68b73384223f4846f465cbe7b4eb0ed152dbd07f
chechugador <chechupareja@gmail.com>
2019-10-08 19:46:34 +0200

    Menú de inicio del juego
    Menú de inicio del juego. Añadida la carpeta Animaciones, con las animaciones que he usado para el menú. También he añadido la carpeta Diseños, con los diseños que he usado para e menú.

---
+++ Assets/Scripts/ChangeScene_Start.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene_Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseClick()
+    {
+        Application.LoadLevel("01-  Single Line TextMesh Pro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
