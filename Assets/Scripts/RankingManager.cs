21dbd4c48358c5181d3bd3e9e6536f377558358b
Marta <marta_centeno_t@hotmail.com>
2019-10-13 13:10:32 +0200

    commit

--- Assets/Scripts/RankingManager.cs
+++ Assets/Scripts/RankingManager.cs
@@ -5,26 +5,11 @@ using UnityEngine;
 
 public class RankingManager : MonoBehaviour
 {
-    public struct Ranked {
-		char[] r_name;
-		unsigned int r_num;
-	}
-	
-	
 	// Start is called before the first frame update
     void Start()
     {
-		private static char [5] name;
-		public static byte[] All_bytes ReadAllBytes(string File_Rank);
-		while(All_bytes){
-			i = 0;
-			while(All_bytes != "*"){
-				name[i] = To.Char (All_bytes[i]);
-				i++;
-				name++;
-			}
-			
-		}
-    } 
+		readRanking();
+	}
 
+	
 }
