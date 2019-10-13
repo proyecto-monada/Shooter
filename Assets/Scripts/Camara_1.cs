6f852e3761f6e835019c1ebd2c687c1fdc7226b2
porto132 <56440710+porto132@users.noreply.github.com>
2019-10-13 21:28:35 +0200

    Movimiento

--- Assets/Scripts/Camara_1.cs
+++ Assets/Scripts/Camara_1.cs
@@ -5,7 +5,7 @@ public class Camara_1 : MonoBehaviour
 {  
     public float sensibilidad = 6.0f;
 
-    private Transform PlayerBody;
+    [SerializeField] private Transform PlayerBody;
     private float posicion;
    
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController characterController;
-    public float speed = 6.0f;
-    public float jumpspeed = 8.0f;
-    public float gravity = 10.0f;
+    public float velocidad = 6.0f;
+    public float salto = 8.0f;
+
+
+    [SerializeField] private AnimationCurve Caida;
+    [SerializeField] private KeyCode jumpkey;
 
     private Vector3 moveDirection = Vector3.zero;
-
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +23,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
+        Movimiento();
+    }
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpspeed;
-            }
-        }
+    void Movimiento()
+    {
+        float ejeY = Input.GetAxis("Vertical") * velocidad;
+        float ejeX = Input.GetAxis("Horizontal") * velocidad;
 
-        moveDirection.y = -gravity * Time.deltaTime;
-        characterController.Move(moveDirection * Time.deltaTime);
+        Vector3 MovVertical = transform.forward * ejeY;
+        Vector3 MovHorizontal = transform.right * ejeX;
+                 
+            characterController.SimpleMove(MovVertical + MovHorizontal);
+
+        InputSalto();
 
     }
+
+    void InputSalto()
+    {
+        if (Input.GetKeyDown(jumpkey) && characterController.isGrounded)
+        {
+            StartCoroutine(Salto());
+        }
+    }
+
+    IEnumerator Salto()
+    {
+        float AirTime = 0.0f;
+        characterController.slopeLimit = 90.0f;
+        do
+        {
+            float FuerzaSalto = Caida.Evaluate(AirTime);
+            characterController.Move(Vector3.up * FuerzaSalto * salto * Time.deltaTime);
+            AirTime += Time.deltaTime;
+
+            yield return null;
+        } while (!characterController.isGrounded && characterController.collisionFlags != CollisionFlags.Above);
+
+        characterController.slopeLimit = 45.0f;
+
+    }
+
 }
